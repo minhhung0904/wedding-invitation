8c2fe9784304548cb0714846e9fe90b2218cc88e
HT <minhhung290701@gmail.com>
2025-12-29 17:25:25 +0700

    .

--- src/component/cover/index.tsx
+++ src/component/cover/index.tsx
@@ -11,9 +11,40 @@ import {
   IMAGE_VAYCUOI,
   IMAGE_BEN1,
   IMAGE_BEN2,
+  IMAGE_CHONGCAM,
+  GALLERY_IMAGES,
+  ALL_PHOTO_IMAGES,
 } from "../../images"
+import { Button } from "../button"
 import { LazyDiv } from "../lazyDiv"
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useMemo, useRef, useState } from "react"
+import ArrowLeft from "../../icons/angle-left-sm.svg?react"
+import { useModal } from "../modal"
+
+const CAROUSEL_ITEMS = ALL_PHOTO_IMAGES.map((item, idx) => (
+  <div className="carousel-item" key={idx}>
+    <img src={item} draggable={false} alt={`${idx}`} />
+  </div>
+))
+
+const DRAG_SENSITIVITY = 15
+
+type Status =
+  | "stationary"
+  | "clicked"
+  | "clickCanceled"
+  | "dragging"
+  | "dragEnding"
+  | "moving-left"
+  | "moving-right"
+
+type DragOption = {
+  startingClientX: number
+  startingClientY: number
+  currentTranslateX: number
+}
+
+type ClickMove = "left" | "right" | null
 
 export const Cover = () => {
   const targetTime = new Date("2026-01-24T12:00:00+07:00").getTime()
@@ -99,6 +130,251 @@ export const Cover = () => {
     }
   }
 
+  const { openModal, closeModal } = useModal()
+  const carouselRef = useRef<HTMLDivElement>({} as HTMLDivElement)
+
+  useEffect(() => {
+    // preload images
+    GALLERY_IMAGES.forEach((image) => {
+      const img = new Image()
+      img.src = image
+    })
+  }, [])
+
+  const [slide, _setSlide] = useState(0)
+  const slideRef = useRef(0)
+  const setSlide = (slide: number) => {
+    _setSlide(slide)
+    slideRef.current = slide
+  }
+
+  const [status, _setStatus] = useState<Status>("stationary")
+  const statusRef = useRef<Status>("stationary")
+  const setStatus = (status: Status) => {
+    _setStatus(status)
+    statusRef.current = status
+  }
+
+  const [dragOption, _setDragOption] = useState<DragOption>({
+    startingClientX: 0,
+    startingClientY: 0,
+    currentTranslateX: 0,
+  })
+  const dragOptionRef = useRef<DragOption>({
+    startingClientX: 0,
+    startingClientY: 0,
+    currentTranslateX: 0,
+  })
+  const setDragOption = (dragOption: DragOption) => {
+    _setDragOption(dragOption)
+    dragOptionRef.current = dragOption
+  }
+
+  const [moveOption, setMoveOption] = useState({
+    srcIdx: 0,
+    dstIdx: 0,
+  })
+
+  const clickMoveRef = useRef<ClickMove>(null)
+  const setClickMove = (clickMove: ClickMove) => {
+    clickMoveRef.current = clickMove
+  }
+
+  // For debugging
+  // useEffect(() => {
+  //   console.log(status)
+  // }, [status])
+
+  const click = (
+    status: Status,
+    clientX: number,
+    clientY: number,
+    carouselWidth: number,
+  ) => {
+    if (status !== "stationary") return
+    setDragOption({
+      startingClientX: clientX,
+      startingClientY: clientY,
+      currentTranslateX: -carouselWidth,
+    })
+    setStatus("clicked")
+  }
+
+  const dragging = useCallback(
+    (dragOption: DragOption, clientX: number, carouselWidth: number) => {
+      let moveTranslateX = clientX - dragOption.startingClientX
+
+      if (moveTranslateX > carouselWidth) {
+        moveTranslateX = carouselWidth
+      } else if (moveTranslateX < -carouselWidth) {
+        moveTranslateX = -carouselWidth
+      }
+
+      setDragOption({
+        ...dragOption,
+        currentTranslateX: moveTranslateX - carouselWidth,
+      })
+    },
+    [],
+  )
+
+  const dragEnd = useCallback(
+    (slide: number, dragOption: DragOption, carouselWidth: number) => {
+      let move = 0
+      if (dragOption.currentTranslateX < -carouselWidth * 1.1) {
+        move = 1
+      } else if (dragOption.currentTranslateX > -carouselWidth * 0.9) {
+        move = -1
+      }
+
+      setDragOption({
+        ...dragOption,
+        currentTranslateX: -carouselWidth * (move + 1),
+      })
+
+      setStatus("dragEnding")
+
+      setTimeout(() => {
+        setDragOption({
+          ...dragOption,
+          currentTranslateX: -carouselWidth,
+        })
+        setStatus("stationary")
+        setSlide((slide + move + CAROUSEL_ITEMS.length) % CAROUSEL_ITEMS.length)
+      }, 300)
+    },
+    [],
+  )
+
+  const move = useCallback((srcIdx: number, dstIdx: number) => {
+    setSlide(dstIdx)
+    if (srcIdx < dstIdx) {
+      setStatus("moving-right")
+    } else {
+      setStatus("moving-left")
+    }
+
+    setMoveOption({ srcIdx, dstIdx })
+
+    setTimeout(() => {
+      setClickMove(null)
+      setStatus("stationary")
+    }, 300)
+  }, [])
+
+  /* Events */
+  const onMouseMove = useCallback(
+    (e: MouseEvent) => {
+      const status = statusRef.current
+
+      if (status === "clicked") {
+        setStatus("dragging")
+      } else if (status === "dragging") {
+        e.preventDefault()
+        dragging(
+          dragOptionRef.current,
+          e.clientX,
+          carouselRef.current.clientWidth,
+        )
+      }
+    },
+    [dragging],
+  )
+
+  const onTouchMove = useCallback(
+    (e: TouchEvent) => {
+      const status = statusRef.current
+
+      if (status === "clicked") {
+        e.preventDefault()
+        const xMove =
+          e.targetTouches[0].clientX - dragOptionRef.current.startingClientX
+        const yMove =
+          e.targetTouches[0].clientY - dragOptionRef.current.startingClientY
+        if (Math.abs(xMove) > DRAG_SENSITIVITY) {
+          setStatus("dragging")
+        } else if (Math.abs(yMove) > DRAG_SENSITIVITY) {
+          setStatus("clickCanceled")
+        }
+      } else if (status === "dragging") {
+        e.preventDefault()
+        dragging(
+          dragOptionRef.current,
+          e.targetTouches[0].clientX,
+          carouselRef.current.clientWidth,
+        )
+      }
+    },
+    [dragging],
+  )
+
+  const onMouseTouchUp = useCallback(() => {
+    const status = statusRef.current
+    const clickMove = clickMoveRef.current
+    const slide = slideRef.current
+
+    if (status === "clicked") {
+      if (clickMove === "left") {
+        move(slide, (slide + CAROUSEL_ITEMS.length - 1) % CAROUSEL_ITEMS.length)
+      } else if (clickMove === "right") {
+        move(slide, (slide + 1) % CAROUSEL_ITEMS.length)
+      } else {
+        setStatus("stationary")
+      }
+    } else if (status === "dragging") {
+      dragEnd(slide, dragOptionRef.current, carouselRef.current.clientWidth)
+    } else if (status === "clickCanceled") {
+      setStatus("stationary")
+    }
+  }, [dragEnd, move])
+
+  useEffect(() => {
+    const carouselElement = carouselRef.current
+
+    window.addEventListener("mousemove", onMouseMove)
+    carouselElement.addEventListener("touchmove", onTouchMove)
+    window.addEventListener("mouseup", onMouseTouchUp)
+    window.addEventListener("touchend", onMouseTouchUp)
+    return () => {
+      window.removeEventListener("mousemove", onMouseMove)
+      carouselElement.removeEventListener("touchmove", onTouchMove)
+      window.removeEventListener("mouseup", onMouseTouchUp)
+      window.removeEventListener("touchend", onMouseTouchUp)
+    }
+  }, [onMouseMove, onTouchMove, onMouseTouchUp])
+
+  const onIndicatorClick = useCallback(
+    (status: Status, srcIdx: number, dstIdx: number) => {
+      if (status !== "stationary" || srcIdx === dstIdx) return
+      move(srcIdx, dstIdx)
+    },
+    [move],
+  )
+
+  const transformStyle = useMemo(() => {
+    switch (status) {
+      case "dragging":
+      case "dragEnding":
+        return { transform: `translateX(${dragOption.currentTranslateX}px)` }
+      default:
+        return {}
+    }
+  }, [status, dragOption])
+
+  const transformClass = useMemo(() => {
+    const className = "carousel-list"
+    switch (status) {
+      case "dragEnding":
+        return className + " transitioning"
+      case "moving-left":
+        return className + " moving-left"
+      case "moving-right":
+        return className + " moving-right"
+      default:
+        return className
+    }
+  }, [status])
+
   return (
     <LazyDiv className="card cover">
       <div className="std">Save The Date</div>
@@ -170,9 +446,7 @@ export const Cover = () => {
           </div>
 
           <div className="invitation">
-            <div className="title-invitation">
-              THAM DỰ LỄ THÀNH HÔN
-            </div>
+            <div className="title-invitation">THAM DỰ LỄ THÀNH HÔN</div>
 
             <div className="subtitle-invitation">Vào lúc</div>
 
@@ -275,12 +549,166 @@ export const Cover = () => {
         </div>
       </div>
       <div className="rectanglered">
-        <img className='ben1' src={IMAGE_BEN1} alt="phukien1" />
-        <img className='vaycuoi' src={IMAGE_VAYCUOI} alt="vaycuoi" />
-        <img className='ben2' src={IMAGE_BEN2} alt="phukien2" />
+        <img className="ben1" src={IMAGE_BEN1} alt="phukien1" />
+        <img className="vaycuoi" src={IMAGE_VAYCUOI} alt="vaycuoi" />
+        <img className="ben2" src={IMAGE_BEN2} alt="phukien2" />
         <span>Hạnh phúc lớn nhất chính là có thể đặt tay mình vào tay em.</span>
       </div>
 
+      <div className="white1">
+        <div className="tendoc">
+          <span className="name"> Hùng</span>
+          <span className="and">&</span>
+          <span className="name"> Trang</span>
+        </div>
+        <img src={IMAGE_CHONGCAM} alt="tvmhttt" />
+      </div>
+
+      <div className="break" />
+
+      <div className="rectanglered2">
+        <span>
+          Em không phải là điểm cuối của tình yêu, mà là động lực nguyên sơ của
+          nó. Vì em, anh đã yêu thế giới này.
+        </span>
+        <div className="card gallery">
+          <h2 className="english">Gallery</h2>
+          <div className="carousel-wrapper">
+            <div
+              className="carousel"
+              ref={carouselRef}
+              onMouseDown={(e) =>
+                click(
+                  statusRef.current,
+                  e.clientX,
+                  e.clientY,
+                  e.currentTarget.clientWidth,
+                )
+              }
+              onTouchStart={(e) =>
+                click(
+                  statusRef.current,
+                  e.targetTouches[0].clientX,
+                  e.targetTouches[0].clientY,
+                  e.currentTarget.clientWidth,
+                )
+              }
+            >
+              <div className={transformClass} style={transformStyle}>
+                {["dragging", "dragEnding"].includes(status) && [
+                  ...(slide === 0
+                    ? [CAROUSEL_ITEMS[CAROUSEL_ITEMS.length - 1]]
+                    : []),
+                  ...CAROUSEL_ITEMS.slice(
+                    slide === 0 ? 0 : slide - 1,
+                    slide + 2,
+                  ),
+                  ...(slide === CAROUSEL_ITEMS.length - 1
+                    ? [CAROUSEL_ITEMS[0]]
+                    : []),
+                ]}
+                {status === "moving-right" &&
+                  CAROUSEL_ITEMS.slice(
+                    moveOption.srcIdx,
+                    moveOption.dstIdx + 1,
+                  )}
+                {status === "moving-left" &&
+                  CAROUSEL_ITEMS.slice(
+                    moveOption.dstIdx,
+                    moveOption.srcIdx + 1,
+                  )}
+                {["stationary", "clicked", "clickCanceled"].includes(status) &&
+                  CAROUSEL_ITEMS[slide]}
+              </div>
+              <div className="carousel-control">
+                <div
+                  className="control left"
+                  onMouseDown={() => {
+                    if (statusRef.current === "stationary") setClickMove("left")
+                  }}
+                  onTouchStart={() => {
+                    if (statusRef.current === "stationary") setClickMove("left")
+                  }}
+                >
+                  <ArrowLeft className="arrow" />
+                </div>
+                <div
+                  className="control right"
+                  onMouseDown={() => {
+                    if (statusRef.current === "stationary")
+                      setClickMove("right")
+                  }}
+                  onTouchStart={() => {
+                    if (statusRef.current === "stationary")
+                      setClickMove("right")
+                  }}
+                >
+                  <ArrowLeft className="arrow right" />
+                </div>
+              </div>
+            </div>
+            <div className="carousel-indicator">
+              {CAROUSEL_ITEMS.map((_, idx) => (
+                <button
+                  key={idx}
+                  className={`indicator${idx === slide ? " active" : ""}`}
+                  onClick={() =>
+                    onIndicatorClick(statusRef.current, slideRef.current, idx)
+                  }
+                />
+              ))}
+            </div>
+          </div>
+
+          <div className="break" />
+
+          <Button
+            onClick={() =>
+              openModal({
+                className: "all-photo-modal",
+                closeOnClickBackground: true,
+                header: <div className="title">Ảnh của chúng mình</div>,
+                content: (
+                  <>
+                    <div className="photo-list">
+                      {GALLERY_IMAGES.map((image, idx) => (
+                        <img
+                          key={idx}
+                          src={image}
+                          alt={`${idx}`}
+                          draggable={false}
+                          onClick={() => {
+                            if (statusRef.current === "stationary") {
+                              if (idx !== slideRef.current) {
+                                move(slideRef.current, idx)
+                              }
+                              closeModal()
+                            }
+                          }}
+                        />
+                      ))}
+                    </div>
+                    <div className="break" />
+                  </>
+                ),
+                footer: (
+                  <Button
+                    buttonStyle="style2"
+                    className="bg-light-grey-color text-dark-color"
+                    onClick={closeModal}
+                  >
+                    Quay lại
+                  </Button>
+                ),
+              })
+            }
+          >
+            Xem tất cả
+          </Button>
+        </div>
+      </div>
+
+      <div className="break" />
 
       {popup.show && (
         <div className="popup-overlay">
--- src/images/index.ts
+++ src/images/index.ts
@@ -1,4 +1,4 @@
-import coverImage from "./wedding/dsc03475.jpg"
+import coverImage from "./wedding/anh25.jpg"
 import imageWellcome from './icon/wellcome.png'
 import imageDate from './icon/date.png'
 import imageGroom from './icon/groom.png'
@@ -22,6 +22,7 @@ import chuhy1 from'./icon/chuhy1.png'
 import vaycuoi from './icon/vaycuoi.png'
 import ben1 from './icon/ben1.png'
 import ben2 from './icon/ben2.png'
+import imagechongcam from './wedding/anh31.jpg'
 
 import image1 from "./image1.png"
 import image2 from "./image2.png"
@@ -60,7 +61,7 @@ export const IMAGE_CHUHY1 = chuhy1
 export const IMAGE_VAYCUOI = vaycuoi
 export const IMAGE_BEN1 = ben1
 export const IMAGE_BEN2 = ben2
-
+export const IMAGE_CHONGCAM = imagechongcam
 
 export const GALLERY_IMAGES = [
   image1,
@@ -75,4 +76,70 @@ export const GALLERY_IMAGES = [
   image10,
   image11,
   image12,
+]
+
+import anh1 from'./wedding/anh1.jpg'
+import anh2 from'./wedding/anh2.jpg'
+import anh3 from'./wedding/anh3.jpg'
+import anh4 from'./wedding/anh4.jpg'
+import anh5 from'./wedding/anh5.jpg'
+import anh6 from'./wedding/anh6.jpg'
+import anh7 from'./wedding/anh7.jpg'
+import anh8 from'./wedding/anh8.jpg'
+import anh9 from'./wedding/anh9.jpg'
+import anh10 from'./wedding/anh10.jpg'
+import anh11 from'./wedding/anh11.jpg'
+import anh12 from'./wedding/anh12.jpg'
+import anh13 from'./wedding/anh13.jpg'
+import anh14 from'./wedding/anh14.jpg'
+import anh15 from'./wedding/anh15.jpg'
+import anh16 from'./wedding/anh16.jpg'
+import anh17 from'./wedding/anh17.jpg'
+import anh18 from'./wedding/anh18.jpg'
+import anh19 from'./wedding/anh19.jpg'
+import anh20 from'./wedding/anh20.jpg'
+import anh21 from'./wedding/anh21.jpg'
+import anh22 from'./wedding/anh22.jpg'
+import anh23 from'./wedding/anh23.jpg'
+import anh24 from'./wedding/anh24.jpg'
+import anh25 from'./wedding/anh25.jpg'
+import anh26 from'./wedding/anh26.jpg'
+import anh27 from'./wedding/anh27.jpg'
+import anh28 from'./wedding/anh28.jpg'
+import anh29 from'./wedding/anh29.jpg'
+import anh30 from'./wedding/anh30.jpg'
+import anh31 from'./wedding/anh31.jpg'
+
+export const ALL_PHOTO_IMAGES = [
+  anh1,
+  anh2,
+  anh3,
+  anh4,
+  anh5,
+  anh6,
+  anh7,
+  anh8,
+  anh9,
+  anh10,
+  anh11,
+  anh12,
+  anh13,
+  anh14,
+  anh15,
+  anh16,
+  anh17,
+  anh18,
+  anh19,
+  anh20,
+  anh21,
+  anh22,
+  anh23,
+  anh24,
+  anh25,
+  anh26,
+  anh27,
+  anh28,
+  anh29,
+  anh30,
+  anh31,
 ]
